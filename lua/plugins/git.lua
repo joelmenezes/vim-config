9dfd5a1ecae03a435d3c3181a7569c289fcdd8e0
Joel  Menezes <joel.menezes@airterra.com>
2024-08-12 22:30:09 -0700

    Add Neogit. Add jdtls.

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -7,5 +7,17 @@ return {
       vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
       vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
     end
+  },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim",
+
+      "nvim-telescope/telescope.nvim", -- optional
+    },
+    config = function()
+      require("neogit").setup {}
+    end
   }
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver" },
+        ensure_installed = { "lua_ls", "tsserver", "jdtls" },
       })
     end,
   },
@@ -24,6 +24,16 @@ return {
       lspconfig.tsserver.setup({
         capabilities = capabilities,
       })
+      lspconfig.jdtls.setup {
+            cmd = {
+                "jdtls",
+                "--jvm-arg=" .. string.format("-javaagent:%s", vim.fn.expand "$MASON/share/jdtls/lombok.jar"),
+            },
+            capabilities = capabilities,
+            handlers = {
+                ["language/status"] = progress_handler,
+            },
+        }
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "def", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
